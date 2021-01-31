bb32020a083d0a3f5119338aaf51a9d18b109c3e
Kirill <utkinkir@gmail.com>
2021-01-31 12:02:31 +0100

    Implement parser command

--- sql_to_code/parsers/__init__.py
+++ sql_to_code/parsers/__init__.py
@@ -1,3 +1,4 @@
 from .alter_parser import alter_parser
-from .table_parser import table_parser
+from .command_parser import parse_commands
 from .enum_parser import enum_parser
+from .table_parser import table_parser
--- sql_to_code/parsers/command_parser.py
+++ sql_to_code/parsers/command_parser.py
@@ -2,11 +2,7 @@ from typing import List
 
 
 def parse_commands(content: str) -> List[str]:
-    raw_commands = content.split('\n\n')
-    commands = [command.replace('\n', '') for command in raw_commands]
+    raw_commands = content.split("\n\n")
+    commands = [command.replace("\n", "") for command in raw_commands]
 
     return commands
-
-
-
-

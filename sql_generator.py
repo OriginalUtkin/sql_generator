b72f7f61ba03a79516be3f931985df0bf462488a
Kirill <utkinkir@gmail.com>
2021-02-06 12:39:18 +0100

    Use global context in entrypoint

--- sql_generator.py
+++ sql_generator.py
@@ -2,8 +2,10 @@ from typing import List
 
 import click
 from sql_to_code.parser_router import get_parser
-from sql_to_code.parsers import ParserOutput
+from sql_to_code.parsers import AlterTable, Enumeration, ParserOutput, Table
 from sql_to_code.utils import get_file_content, parse_commands
+
+from .models import Context, TableContext
 
 
 @click.command()
@@ -12,13 +14,17 @@ def generate_models(input_sql: str) -> None:
     file_content: str = get_file_content(filename=input_sql)
     raw_sql_commands: List[str] = parse_commands(content=file_content)
 
-    context: List[ParserOutput] = []
+    context = Context()
 
     for command in raw_sql_commands:
         parser = get_parser(sql_text=command)
         result: ParserOutput = parser.parse(command)
 
-        context.append(result)
+        if isinstance(result, Table):
+            context.tables.append(TableContext(table=result))
+
+        if isinstance(result, Enumeration):
+            context.enums.append(result)
 
 
 if __name__ == "__main__":

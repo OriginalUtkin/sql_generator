0cefb6a3f4470b9d908fc3481873268e03489910
Kirill <utkinkir@gmail.com>
2021-02-06 11:33:22 +0100

    Adjust isort configuration

--- sql_generator.py
+++ sql_generator.py
@@ -1,12 +1,13 @@
-import click
-from sql_to_code.parsers import ParserOutput
-from sql_to_code.parser_router import get_parser
-from sql_to_code.utils import get_file_content, parse_commands
 from typing import List
+
+import click
+from sql_to_code.parser_router import get_parser
+from sql_to_code.parsers import ParserOutput
+from sql_to_code.utils import get_file_content, parse_commands
 
 
 @click.command()
-@click.option('--input_sql', help="Path to file with raw sql commands.")
+@click.option("--input_sql", help="Path to file with raw sql commands.")
 def generate_models(input_sql: str) -> None:
     file_content: str = get_file_content(filename=input_sql)
     raw_sql_commands: List[str] = parse_commands(content=file_content)

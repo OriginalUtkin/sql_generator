5ef5f291e0466fcbfb0fd3c3750e66feeda47439
Kirill <utkinkir@gmail.com>
2021-02-06 11:26:37 +0100

    Define parsers output

--- sql_to_code/parsers/__init__.py
+++ sql_to_code/parsers/__init__.py
@@ -1,1 +1,8 @@
+from typing import Union
+
 from . import alter_table, create_enum, create_table
+from .alter_table.models import AlterTableContext
+from .create_enum.models import EnumContext
+from .create_table.models import Table
+
+ParserOutput = Union[Table, EnumContext, AlterTableContext]

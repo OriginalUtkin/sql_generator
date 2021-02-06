d31b0a0b65f48e55e765643982db38516240b0d9
Kirill <utkinkir@gmail.com>
2021-02-06 12:38:44 +0100

    Define global context

---
+++ models.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+from typing import List
+from sql_to_code.parsers.create_table.models import Table
+from sql_to_code.parsers.alter_table.models import AlterTable
+from sql_to_code.parsers.create_enum.models import Enumeration
+
+
+@dataclass
+class TableContext:
+    table: Table
+    alter_tables: List[AlterTable] = field(default_factory=list)
+
+
+@dataclass
+class Context:
+    tables: List[TableContext] = field(default_factory=list)
+    enums: List[Enumeration] = field(default_factory=list)

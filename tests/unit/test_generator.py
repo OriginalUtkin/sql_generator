f1cf25c28580d14c893e80be6a9e680bd3bc03b4
vlado <vladimir.chernenko@kiwi.com>
2021-02-06 15:14:24 +0100

    update test

--- tests/unit/test_generator.py
+++ tests/unit/test_generator.py
@@ -1,5 +1,11 @@
+import os
+
 from sql_to_code.generator import generate_models
+
+test_file_name = "_result_models.py"
 
 
 def test_generate_models():
-    generate_models("tests/test_sql/test_schema.sql")
+    generate_models("tests/test_sql/test_schema.sql", "sqlalchemy", test_file_name)
+
+    os.remove(test_file_name)
